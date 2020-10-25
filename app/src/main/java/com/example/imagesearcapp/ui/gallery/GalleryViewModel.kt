55d49cdc437cbcedbbddf16e779c90fd9f3175b7
pessel <pessel@jimmyjohns.com>
2020-10-25 03:30:26 -0400

    Handles process death.

--- app/src/main/java/com/example/imagesearcapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/imagesearcapp/ui/gallery/GalleryViewModel.kt
@@ -1,18 +1,17 @@
 package com.example.imagesearcapp.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.example.imagesearcapp.data.Repository
 
 class GalleryViewModel@ViewModelInject constructor(
-    private  val repository: Repository
+    private  val repository: Repository,
+    @Assisted state: SavedStateHandle
 ): ViewModel() {
 
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
     val photos = currentQuery.switchMap {queryString ->
         repository.getSearchResults(queryString).cachedIn(viewModelScope)
@@ -25,5 +24,6 @@ class GalleryViewModel@ViewModelInject constructor(
 
     companion object {
         private  const val DEFAULT_QUERY = "cats"
+        private  const val CURRENT_QUERY = "current-query"
     }
 }
